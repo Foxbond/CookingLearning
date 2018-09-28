78fbdfeee41505d157577f60543aaa7e72a5ab99
Michał Chraniuk <foxbond@o2.pl>
2018-09-28 14:49:24 +0200

    Basic js (animate features menu)

--- script.js
+++ script.js
@@ -1,7 +1,26 @@
+var currentFeature = 'basic';
 
+
+function featureHandler (ev){
+	selectedFeature = $(this).parent().attr('id').substr(15);
+	
+	if (selectedFeature == currentFeature) {
+		$('.recipe_feature_body').slideUp();
+		currentFeature = 'basic'
+		return;
+	}
+	
+	$('.recipe_feature_body').slideUp();
+	$('#recipe_feature_'+selectedFeature+' > .recipe_feature_body').slideDown();
+	
+	currentFeature = selectedFeature;
+	
+}
 
 
 
 $(document).ready(function(){
+	
+	$('.recipe_feature_name').on('click', featureHandler);
 	
 });
