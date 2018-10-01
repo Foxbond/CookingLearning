444f830a467e5fbac08aaba71b838a43019fa18e
Michał Chraniuk <foxbond@o2.pl>
2018-10-01 12:34:03 +0200

    Cleanup featureHandler

--- script.js
+++ script.js
@@ -4,13 +4,13 @@ var currentFeature = 'basic';
 function featureHandler (ev){
 	selectedFeature = $(this).parent().attr('id').substr(15);
 	
+	$('.recipe_feature_body').slideUp();
+	
 	if (selectedFeature == currentFeature) {
-		$('.recipe_feature_body').slideUp();
 		currentFeature = 'basic'
 		return;
 	}
 	
-	$('.recipe_feature_body').slideUp();
 	$('#recipe_feature_'+selectedFeature+' > .recipe_feature_body').slideDown();
 	
 	currentFeature = selectedFeature;
