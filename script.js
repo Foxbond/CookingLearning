bfd363191f51d254aa3d9eb019a13a3987fb49b3
Michał Chraniuk <foxbond@o2.pl>
2018-10-01 13:14:00 +0200

    Add comments

--- script.js
+++ script.js
@@ -1,11 +1,12 @@
-var currentFeature = 'basic';
+var currentFeature = 'basic'; /* We need to track which feature is currently active */
 
 
 function featureHandler (ev){
-	selectedFeature = $(this).parent().attr('id').substr(15);
+	selectedFeature = $(this).parent().attr('id').substr(15); /* extract selected feature name from id attr of parent */
 	
-	$('.recipe_feature_body').slideUp();
+	$('.recipe_feature_body').slideUp(); 
 	
+	//User clicked on currently selected feature
 	if (selectedFeature == currentFeature) {
 		currentFeature = 'basic'
 		return;
@@ -18,7 +19,7 @@ function featureHandler (ev){
 }
 
 
-
+//It should wait until entire page is loaded
 $(document).ready(function(){
 	
 	$('.recipe_feature_name').on('click', featureHandler);
